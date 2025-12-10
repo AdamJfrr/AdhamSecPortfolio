b2b2e8fe3098fcb6487cf2789edce9b4fc87724a
adhamj <adham-jaafar@outlook.com>
2025-12-10 18:13:48 +0200

    added new project

--- src/components/ProjectRow.js
+++ src/components/ProjectRow.js
@@ -9,8 +9,7 @@ const ProjectRow = (props) => {
       <div className={classes.row}>
         <p className={classes.projectName}>{props.name}</p>
         <p className={classes.projectDescription}>
-          Comprehensive vulnerability assessment and digital forensic analysis on simulated IT infrastructure, security risks, and threat artifacts,
-          including a structured VAPT report with risk classifications, technical findings, remediation recommendations, and professional documentation.
+          {props.description}
         </p>
         <span className={classes.arrow}>&gt;</span>
       </div>
--- src/components/ProjectSection.js
+++ src/components/ProjectSection.js
@@ -6,7 +6,10 @@ const ProjectSection = ()=>{
     <div className={classes.project_section}>
       <div className={classes.header}><p className={classes.title}>Projects</p></div>
       <div className={classes.content}>
-        <ProjectRow name='TechShield Vulnerability Assessment' githubLink="https://github.com/AdamJfrr/VAPT-Report-TechShield"/>
+        <ProjectRow name='TechShield Vulnerability Assessment'
+        description='Comprehensive vulnerability assessment and digital forensic analysis on simulated IT infrastructure, security risks, and threat artifacts, including a structured VAPT report with risk classifications, technical findings, remediation recommendations, and professional documentation.'githubLink="https://github.com/AdamJfrr/VAPT-Report-TechShield"/>
+        <ProjectRow name='SOC Detection Lab'
+        description='Interactive portfolio showcasing 14 production-ready Splunk detection rules mapped to MITRE ATT&CK. Demonstrates advanced SPL proficiency, alert engineering, and threat detection across 6 tactics'githubLink="https://AdamJfrr.github.io/soc-detection-lab"/>
       </div>
     </div>
   )
